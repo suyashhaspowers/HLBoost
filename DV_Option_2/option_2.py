bbf5cd357afbcf27e81da17f215ae091f2017c4e
suyashhaspowers <suyashhaspowers@gmail.com>
2022-07-08 02:17:50 -0400

    fixed spelling

--- DV_Option_2/option_2.py
+++ DV_Option_2/option_2.py
@@ -5,9 +5,9 @@ def generate_design_option_two(df, length):
     path = Path("option_2.py")
     upper_directory_path = str(path.parent.absolute())
 
-    fig = px.scatter(x=length, y=df['light'], size=df["productivity"], error_y=df['std_light'], labels=dict(x="Session Number", y="Light Values (Lumens)", size="Producitivity Rating"))
+    fig = px.scatter(x=length, y=df['light'], size=df["productivity"], error_y=df['std_light'], labels=dict(x="Session Number", y="Light Values (Lumens)", size="Productivity Rating"))
     fig.update_layout(title="Light Values VS. Study Session Number")
     fig.write_image(upper_directory_path + "/static/designoption2fig1.png")
-    fig = px.scatter(x=length, y=df['humidity'], size=df["productivity"], error_y=df['std_humidity'], labels=dict(x="Session Number", y="Humidity Values (%)", size="Producitivity Rating"))
+    fig = px.scatter(x=length, y=df['humidity'], size=df["productivity"], error_y=df['std_humidity'], labels=dict(x="Session Number", y="Humidity Values (%)", size="Productivity Rating"))
     fig.update_layout(title="Humidity Values VS. Study Session Number")
     fig.write_image(upper_directory_path + "/static/designoption2fig2.png")
