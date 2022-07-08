fe1b31d05503d229b15447de817da7b6e44a92ed
Archan Patel <archanmehulpatel@gmail.com>
2022-07-08 03:29:56 -0400

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import false
+import pickle
 
 from DV_Option_1.option_1 import generate_design_option_one
 from DV_Option_2.option_2 import generate_design_option_two
@@ -10,7 +11,7 @@ app = Flask(__name__)
 
 IS_LAST_SESSION_RATED = False # Default for now
 CSV_FILE_NAME = 'test.csv'
-
+productivityResult = 0
 def read_csv():
     df = pd.read_csv(CSV_FILE_NAME, index_col=0)
     return df
@@ -35,18 +36,51 @@ def get_length_df(df):
         length.append(cur)
         cur += 1
 
+# run this route at launch
 @app.route('/')
 def start():
     df = read_csv()
     is_last_session_rated = determine_if_last_session_rated(df)
-    df = update_last_session_rating(is_last_session_rated, df,2)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult)
+    return render_template('homepage.html', is_last_session_rated=is_last_session_rated)
+
+#launch the questionaire
+@app.route('/dashboard')
+def index():
+    df = read_csv()
+    is_last_session_rated = determine_if_last_session_rated(df)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult)
     return render_template('index.html', is_last_session_rated=is_last_session_rated)
+
+# show the productivity score after completing the study session questionaire
+@app.route('/productivityResult', methods=['GET', 'POST']) #obtain the required information using http requests
+def productivityResult():
+    # using the post method get the required information from the questionaire using the name tag of the input fields
+    if request.method == 'POST':
+        stressStart = request.form.get('stressSelect')
+        stressEnd = request.form.get('stressSelectEnd')
+        energyStart = request.form.get('energySelect')
+        energyEnd = request.form.get('energySelectEnd')
+
+        # calculate a productivity score for the user inputed data
+        productivity = stressStart - stressEnd + (5-energyStart-energyEnd)
+        if productivity>6:
+            productivityResult = 3
+        elif productivity>3:
+            productivityResult = 2
+        else:
+            productivityResult = 1
+            
+    df = read_csv()
+    is_last_session_rated = determine_if_last_session_rated(df)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult) #pass the productivity score to the DV csv
+    return render_template('productivityResult.html', is_last_session_rated=is_last_session_rated)
 
 @app.route('/design-option-1')
 def design_option_1():
     df = read_csv()
     is_last_session_rated = determine_if_last_session_rated(df)
-    df = update_last_session_rating(is_last_session_rated, df,2)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult)
     length = get_length_df(df)
     generate_design_option_one(df, length)
     return render_template('designoption1.html', is_last_session_rated=is_last_session_rated)
@@ -55,7 +89,7 @@ def design_option_1():
 def design_option_2():
     df = read_csv()
     is_last_session_rated = determine_if_last_session_rated(df)
-    df = update_last_session_rating(is_last_session_rated, df,2)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult)
     length = get_length_df(df)
     generate_design_option_two(df, length)
     return render_template('designoption2.html', is_last_session_rated=is_last_session_rated)
@@ -64,6 +98,6 @@ def design_option_2():
 def design_option_3():
     df = read_csv()
     is_last_session_rated = determine_if_last_session_rated(df)
-    df = update_last_session_rating(is_last_session_rated, df,2)
+    df = update_last_session_rating(is_last_session_rated, df,productivityResult)
     generate_design_option_three(df)
-    return render_template('index.html', is_last_session_rated=is_last_session_rated)
+    return render_template('designoption3.html', is_last_session_rated=is_last_session_rated)
